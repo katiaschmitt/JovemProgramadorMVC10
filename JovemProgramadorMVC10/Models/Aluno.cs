b7f7f577797cb8a989d9f8ef94b7fd981cc4a91d
katiaschmitt <katiaschmittp@gmail.com>
2024-08-13 21:52:24 -0300

    Criado classe Aluno

---
+++ JovemProgramadorMVC10/Models/Aluno.cs
@@ -0,0 +1,9 @@
+﻿namespace JovemProgramadorMVC10.Models
+{
+    public class Aluno
+    {
+        public string Matricula { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+    }
+}
